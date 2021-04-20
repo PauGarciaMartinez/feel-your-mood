c315aa817c6821d98baa891652764cb69a977fbc
Pau Garcia Martínez <etpau@hotmail.es>
2021-04-20 22:47:36 +0200

    Add response options for changing mood values

--- src/components/Recommendation.vue
+++ src/components/Recommendation.vue
@@ -1,8 +1,14 @@
 <!-- TEMPLATE -->
 <template>
-  <h2>Hey</h2>
 
-  {{ moodValue }}
+  <h3 v-if="moodValue <= 50">Hey... things are gonna be fine!</h3>
+  <h3 v-else-if="moodValue > 50 && moodValue <= 100">C'mon, it's not a great day... but you're alright!</h3>
+  <h3 v-else-if="moodValue > 100 && moodValue <= 150">Well... that's pretty good, actually</h3>
+  <h3 v-else-if="moodValue > 150 && moodValue <= 200">Well... that's pretty good, actually</h3>
+  <h3 v-else-if="moodValue > 200 && moodValue <= 250">Ouuu yeah! Let's eat the world today!</h3>
+  <h3 v-else-if="moodValue > 250 && moodValue <= 300">You're overpowered!!! Ecstasy ON!</h3>
+
+  {{ result }}
 
 </template>
 
--- src/scripts/recommendation.js
+++ src/scripts/recommendation.js
@@ -3,9 +3,36 @@ export default {
   props: ['moodValue'],
   data() {
     return {
+      result: '',
+      responseAvailable: false,
+      apiKey: '426aef7e46msh1efabc60fe00542p157833jsn436bd09d04b2'
     }
   },
-  components: {
-    
+  methods: {
+    fetchAPIData() {
+      this.responseAvailable = false;
+
+      fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movies-by-title&title=matrix", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-key": this.apiKey,
+          "x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com"
+        }
+      })
+      .then(response => { 
+          if(response.ok){
+              return response.json()    
+          } else{
+              alert("Server returned " + response.status + " : " + response.statusText);
+          }                
+      })
+      .then(response => {
+          this.result = response.body; 
+          this.responseAvailable = true;
+      })
+      .catch(err => {
+          console.log(err);
+      });
+    }
   }
 }
