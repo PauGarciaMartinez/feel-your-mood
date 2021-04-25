1f05d6912acd0a2049ac4cad2d720605d0406035
paugarciamartinez <etpau@hotmail.es>
2021-04-25 20:18:01 +0200

    Add btn to fetch again

--- src/components/Recommendation.vue
+++ src/components/Recommendation.vue
@@ -28,6 +28,8 @@
 
   <button class="main-btn back-btn" @click="goBack">Change the mood</button>
   
+  <button class="main-btn back-btn" @click="fetchMovie">Another one</button>
+  
 </template>
 
 <!-- SCRIPT -->
